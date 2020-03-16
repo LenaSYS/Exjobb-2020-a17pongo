3015f56bb914c850aaa4156edbc459c1781dcd22
Redines <pontus.g91@gmail.com>
2020-03-16 10:11:10 +0100

    added test 3D object to scen

--- main.js
+++ main.js
@@ -2,8 +2,8 @@ console.log("this script is run");
 
 //Setting up threejs
 const FOV = 45;
-const WIDTH = 640;
-const HEIGHT = 580;
+const WIDTH = 800;
+const HEIGHT = 600;
 const ASPECT = WIDTH / HEIGHT;
 const NEAR = 0.1;
 const FAR = 10000;
@@ -14,3 +14,11 @@ const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
 var renderer = new THREE.WebGLRenderer();
 renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
+
+//adding a 3D object
+var geometry = new THREE.BoxGeometry();
+var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
+var cube = new THREE.Mesh( geometry, material );
+scene.add( cube );
+
+camera.position.z = 5;
