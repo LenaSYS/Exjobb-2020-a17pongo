f09310ad8b94ecbdbfac7d73a08f81911dad8676
Redines <pontus.g91@gmail.com>
2020-03-16 09:59:33 +0100

    added threejs 3D scen, camera and renderer

--- main.js
+++ main.js
@@ -1,1 +1,16 @@
 console.log("this script is run");
+
+//Setting up threejs
+const FOV = 45;
+const WIDTH = 640;
+const HEIGHT = 580;
+const ASPECT = WIDTH / HEIGHT;
+const NEAR = 0.1;
+const FAR = 10000;
+
+var scene = new THREE.Scene();
+const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+
+var renderer = new THREE.WebGLRenderer();
+renderer.setSize( window.innerWidth, window.innerHeight );
+document.body.appendChild( renderer.domElement );
