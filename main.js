f0f0596805b70c3f34cb920ad8c65500ff363214
Redines <pontus.g91@gmail.com>
2020-03-16 11:20:17 +0100

    made 3D object show wireframe for more clear visual of a 3D object

--- main.js
+++ main.js
@@ -16,18 +16,23 @@ renderer.setSize( window.innerWidth, window.innerHeight );
 document.body.appendChild( renderer.domElement );
 
 //adding a 3D object
-var geometry = new THREE.BoxGeometry();
-var material = new THREE.MeshBasicMaterial( { color: 0x00ff00 } );
-var cube = new THREE.Mesh( geometry, material );
-scene.add( cube );
+var geometry = new THREE.CylinderBufferGeometry( 5, 5, 20, 32 );
+var material = new THREE.MeshBasicMaterial( {color: 0x0000ff,wireframe:true} );
+var cylinder = new THREE.Mesh( geometry, material );
+scene.add( cylinder );
 
-camera.position.z = 5;
+camera.position.z = 100;
+
+//adding light source to scene
+const light = new THREE.PointLight(0xffffff, 1.2);
+light.position.set(0, 0, 6);
+scene.add(light);
 
 //Animate funktion to draw 3D object to scene
 function animate() {
 	requestAnimationFrame( animate );
     renderer.render( scene, camera );
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
+    cylinder.rotation.x += 0.01;
+    cylinder.rotation.y += 0.01;
 }
 animate();
