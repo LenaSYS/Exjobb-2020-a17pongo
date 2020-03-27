20ac10882428f2257a8a9bcded552f0e611861e2
Redines <pontus.g91@gmail.com>
2020-03-27 12:45:17 +0100

    deleted test file, fixed simplified data to correct unicode, fixed åäö in city names in csv file, removed code writing to test.json

--- csvTojson/csvTojson.py
+++ csvTojson/csvTojson.py
@@ -10,11 +10,11 @@ def strip(text):
         return text
 
 #Read CSV file content and store in dataFrame, also converts City column in data to a new string with no whitespace
-df = pd.read_csv(dataFile, encoding='utf8', converters = {'City' : strip})
+df = pd.read_csv(dataFile, encoding='utf_8', converters = {'City' : strip})
 
 #Print to se data result, used for testing
 print(df.columns)
 print(df)
 
 #print restructured data to json format
-df.to_json("data/test.json", orient="records")
+df.to_json("data/2018_SCB_BEFOLK_MANGD_simplified_data.json", force_ascii=False, orient="records")
