2e4cb6e7f08baf1824c22ed97ccef34f4918da77
Redines <pontus.g91@gmail.com>
2020-03-27 12:07:34 +0100

    added orient function to get correct json format as following: [{city,population},{..}]

--- csvTojson/csvTojson.py
+++ csvTojson/csvTojson.py
@@ -12,9 +12,9 @@ def strip(text):
 #Read CSV file content and store in dataFrame, also converts City column in data to a new string with no whitespace
 df = pd.read_csv(dataFile, encoding='utf8', converters = {'City' : strip})
 
-#Print to se data result and write to json file
+#Print to se data result, used for testing
 print(df.columns)
 print(df)
 
-
-#print(df.head(10))
+#print restructured data to json format
+df.to_json("data/test.json", orient="records")
