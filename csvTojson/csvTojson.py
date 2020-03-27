98b38fe9899d88329d98e210292d9771f7fc279b
Redines <pontus.g91@gmail.com>
2020-03-27 11:47:12 +0100

    added function to remove whitespace from City column in simplified dataset using converters

--- csvTojson/csvTojson.py
+++ csvTojson/csvTojson.py
@@ -2,8 +2,19 @@ import pandas as pd
 
 dataFile = 'data/2018_SCB_BEFOLK_MANGD_simplified_data.csv'
 
-#Read CSV file content and store in dataFrame
-df = pd.read_csv(dataFile, encoding='utf8')
+#Function for cleaning up empty whitespace from dataset
+def strip(text):
+    try:
+        return text.strip()
+    except AttributeError:
+        return text
+
+#Read CSV file content and store in dataFrame, also converts City column in data to a new string with no whitespace
+df = pd.read_csv(dataFile, encoding='utf8', converters = {'City' : strip})
 
 #Print to se data result and write to json file
-print(df.tail(10))
+print(df.columns)
+print(df)
+
+
+#print(df.head(10))
