5f08c0820a29b1acbf05e7d569fb388c19e56770
Redines <pontus.g91@gmail.com>
2020-03-30 18:28:12 +0200

    added string to int converstion for data to remove and structure the int before writing ti .json file, added .gitignore to not commit unwanted files to github

--- csvTojson/csvTojson.py
+++ csvTojson/csvTojson.py
@@ -10,11 +10,14 @@ def strip(text):
         return text
 
 #Read CSV file content and store in dataFrame, also converts City column in data to a new string with no whitespace
-df = pd.read_csv(dataFile, encoding='utf_8', converters = {'City' : strip})
+df = pd.read_csv(dataFile, encoding='utf-8', converters = {'City' : strip})
 
 #Print to se data result, used for testing
-print(df.columns)
-print(df)
+print(df["Population"]) #before manip
+extr = df.Population.str.replace(',', '')
+
+print(extr.astype(int).head(10)) #after manip
+df['Population'] = extr.astype(int) #converting finish manip data to int
 
 #print restructured data to json format
-df.to_json("data/2018_SCB_BEFOLK_MANGD_simplified_data.json", force_ascii=False, orient="records")
+df.to_json("data/2018_SCB_BEFOLK_MANGD_simplified_data.json", force_ascii=True, orient="records")
