a746c44b5cc93686f3ecf58bf7ef353771184524
Redines <pontus.g91@gmail.com>
2020-04-06 12:18:37 +0200

    renamed canvas element to something more clear, added a threejs test plane with green color (will be replaced with a map), made various small changes and removed unnecesarry code

--- index.js
+++ index.js
@@ -24,26 +24,34 @@ L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}',
 function threejs_init(){
     //Setting up threejs
     const FOV = 45;
-    const WIDTH = 800;
-    const HEIGHT = 600;
+    const WIDTH = window.innerWidth;
+    const HEIGHT = window.innerHeight;
     const ASPECT = WIDTH / HEIGHT;
     const NEAR = 0.1;
     const FAR = 10000;
 
     var scene = new THREE.Scene();
     const camera = new THREE.PerspectiveCamera(FOV, ASPECT, NEAR, FAR);
+    var canvas = document.getElementById("map");
+    canvas.width = window.innerWidth;
+    canvas.height = window.innerHeight;
+    const renderer = new THREE.WebGLRenderer({ alpha: true, canvas: canvas });
 
-    var renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    //Adding plane for showing the leaflet map
+    var pm = new THREE.MeshBasicMaterial({color:0x57a53c});
+    var pg = new THREE.PlaneBufferGeometry( 20000, 20000 );
+    var mesh = new THREE.Mesh(pg , pm );
+    mesh.position.y = - 250;
+    mesh.rotation.x = - Math.PI / 2;
+    scene.add( mesh );
 
     //adding a 3D object
     var geometry = new THREE.CylinderBufferGeometry( 5, 5, 20, 32 );
-    var material = new THREE.MeshBasicMaterial( {color: 0x0000ff,wireframe:true} );
+    var material = new THREE.MeshBasicMaterial( {color: 0x0000ff,wireframe:false} );
     var cylinder = new THREE.Mesh( geometry, material );
     scene.add( cylinder );
 
-    camera.position.z = 100;
+    camera.position.z = 50;
 
     //adding light source to scene
     const light = new THREE.PointLight(0xffffff, 1.2);
@@ -54,8 +62,6 @@ function threejs_init(){
     function animate() {
         requestAnimationFrame( animate );
         renderer.render( scene, camera );
-        cylinder.rotation.x += 0.01;
-        cylinder.rotation.y += 0.01;
     }
     animate();
 }
