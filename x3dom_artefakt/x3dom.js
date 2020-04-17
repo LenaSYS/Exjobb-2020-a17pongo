629ed06d92f71818acaca522a29a425ecc959934
Redines <pontus.g91@gmail.com>
2020-04-17 10:35:10 +0200

    moved code to add cylinders inside new addCylinders function, moved function calls to the bottom of the file

--- x3dom_artefakt/x3dom.js
+++ x3dom_artefakt/x3dom.js
@@ -11,32 +11,32 @@ function fetchData(){
   });
 }
 
-fetchData();
-
 //function for dynamicly add cylinders to scene
 function addCylinders(){
+    var t = document.createElement('Transform');
+    t.setAttribute("translation","2 -0.5 1" );
+    var s = document.createElement('Shape');
 
+    // Appearance Node
+    var app = document.createElement('Appearance');
+
+    // Material Node
+    var mat = document.createElement('Material');
+
+    app.appendChild(mat);
+
+    s.appendChild(app);
+
+    t.appendChild(s);
+    var b = document.createElement('Cylinder');
+    b.setAttribute("radius",0.1)
+    b.setAttribute("height",1)
+    s.appendChild(b);
+
+    var ot = document.getElementById('root');
+    ot.appendChild(t);
 }
 
-var t = document.createElement('Transform');
-t.setAttribute("translation","2 -0.5 1" );
-var s = document.createElement('Shape');
 
-// Appearance Node
-var app = document.createElement('Appearance');
-
-// Material Node
-var mat = document.createElement('Material');
-
-app.appendChild(mat);
-
-s.appendChild(app);
-
-t.appendChild(s);
-var b = document.createElement('Cylinder');
-b.setAttribute("radius",0.1)
-b.setAttribute("height",1)
-s.appendChild(b);
-
-var ot = document.getElementById('root');
-ot.appendChild(t);
+fetchData();
+addCylinders();
