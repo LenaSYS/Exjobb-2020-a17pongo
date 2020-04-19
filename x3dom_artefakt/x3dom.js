2d3703453f8ce2fafdbe4e589abffba665dd38ef
redines <pontus.g91@gmail.com>
2020-04-19 15:55:17 +0200

    commented out for loop for testing purposes of new for loop, added chanceJS library for easier random number generation

--- x3dom_artefakt/x3dom.js
+++ x3dom_artefakt/x3dom.js
@@ -24,8 +24,8 @@ function fetchData(){
 //function for dynamicly add cylinders to scene
 function addCylinders(data){
     
-    //top of sweden
-    for(var i = 0; i<data.length; i++){
+    //bottom of sweden
+    for(var i = 0; i< 10; i++){
       // Material Node
       var mat = document.createElement('Material');
       // Appearance Node
@@ -44,8 +44,10 @@ function addCylinders(data){
       app.appendChild(mat);
     }
 
-      var xCord = Math.floor(Math.random() * 6 - 3);
-      var zcord = Math.floor(Math.random() * 3);
+    var xCord = Math.floor(Math.random() * 6 - 3);
+    var zcord = Math.floor(Math.random() * 3 - 5);
+
+    console.log(zcord)
 
         var t = document.createElement('Transform');
         t.setAttribute("translation", xCord + " -0.5 " + zcord );
@@ -62,6 +64,46 @@ function addCylinders(data){
         var ot = document.getElementById('root');
         ot.appendChild(t);
     }
+
+        //top of sweden
+       /* for(var i = 0; i< 50; i++){
+          // Material Node
+          var mat = document.createElement('Material');
+          // Appearance Node
+          var app = document.createElement('Appearance');
+    
+        if(data[i].year > 700){
+            mat.setAttribute("diffuseColor",red);
+            app.appendChild(mat);
+        }
+        else if(data[i].year > 500){
+          mat.setAttribute("diffuseColor",yellow);
+          app.appendChild(mat);
+        }
+        else{
+          mat.setAttribute("diffuseColor",green);
+          app.appendChild(mat);
+        }
+    
+        var xCord = Math.floor(Math.random() * 6 - 3);
+        var zcord = Math.floor(Math.random() * 3);
+    
+            var t = document.createElement('Transform');
+            t.setAttribute("translation", xCord + " -0.5 " + zcord );
+            var s = document.createElement('Shape');
+    
+            s.appendChild(app);
+    
+            t.appendChild(s);
+            var b = document.createElement('Cylinder');
+            b.setAttribute("radius",0.1)
+            b.setAttribute("height",0.55)
+            s.appendChild(b);
+    
+            var ot = document.getElementById('root');
+            ot.appendChild(t);
+        }*/
+
 }
 
 
