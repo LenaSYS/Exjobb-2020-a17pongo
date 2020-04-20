85f40867e41f6d3c6e0797f995e6ef64bf4e0288
Redines <pontus.g91@gmail.com>
2020-04-20 09:11:43 +0200

    formated x3dom.js file strukture

--- x3dom_artefakt/x3dom.js
+++ x3dom_artefakt/x3dom.js
@@ -1,104 +1,104 @@
-console.log("Running x3dom artefakt");
+console.log('Running x3dom artefakt');
 
 //Constant RGB color values for coloring cylinders depending on data value
 //RED: alot of data
 //yellow: medium amount of data
 //green: not alot of data
 
-const red = "#ff0000";
-const yellow = "#f9b700";
-const green = "#86ea34";
+const red = '#ff0000';
+const yellow = '#f9b700';
+const green = '#86ea34';
 
 //function for retrieving data from json file with fetch API
-function fetchData(){
-    fetch('../data/SMHI_merged_simplified_data.json')
-  .then((response) => {
-    return response.json();
-  })
-  .then((data) => {
-    console.log(data)
-    addCylinders(data)
-  });
+function fetchData() {
+	fetch('../data/SMHI_merged_simplified_data.json')
+		.then((response) => {
+			return response.json();
+		})
+		.then((data) => {
+			console.log(data);
+			addCylinders(data);
+		});
 }
 
 //function for dynamicly add cylinders to scene
-function addCylinders(data){
-  var xCord = new Chance();
-  var yCord = new Chance();
-    
-    //bottom of sweden
-    for(var i = 0; i< 500; i++){
-      // Material Node
-      var mat = document.createElement('Material');
-      // Appearance Node
-      var app = document.createElement('Appearance');
+function addCylinders(data) {
+	var xCord = new Chance();
+	var yCord = new Chance();
 
-    if(data[i].year > 700){
-        mat.setAttribute("diffuseColor",red);
-        app.appendChild(mat);
-    }
-    else if(data[i].year > 500){
-      mat.setAttribute("diffuseColor",yellow);
-      app.appendChild(mat);
-    }
-    else{
-      mat.setAttribute("diffuseColor",green);
-      app.appendChild(mat);
-    }
+	//bottom of sweden
+	for (var i = 0; i < 500; i++) {
+		// Material Node
+		var mat = document.createElement('Material');
+		// Appearance Node
+		var app = document.createElement('Appearance');
 
-        var t = document.createElement('Transform');
-        t.setAttribute("translation", xCord.integer({min: -2.5, max: 10}) + " -0.5 " + yCord.integer({min: -5, max: 10}) );
-        var s = document.createElement('Shape');
+		if (data[i].year > 700) {
+			mat.setAttribute('diffuseColor', red);
+			app.appendChild(mat);
+		} else if (data[i].year > 500) {
+			mat.setAttribute('diffuseColor', yellow);
+			app.appendChild(mat);
+		} else {
+			mat.setAttribute('diffuseColor', green);
+			app.appendChild(mat);
+		}
 
-        s.appendChild(app);
+		var t = document.createElement('Transform');
+		t.setAttribute(
+			'translation',
+			xCord.integer({ min: -2.5, max: 10 }) + ' -0.5 ' + yCord.integer({ min: -5, max: 10 })
+		);
+		var s = document.createElement('Shape');
 
-        t.appendChild(s);
-        var b = document.createElement('Cylinder');
-        b.setAttribute("radius",0.01)
-        b.setAttribute("height",0.55)
-        s.appendChild(b);
+		s.appendChild(app);
 
-        var ot = document.getElementById('root');
-        ot.appendChild(t);
-    }
+		t.appendChild(s);
+		var b = document.createElement('Cylinder');
+		b.setAttribute('radius', 0.01);
+		b.setAttribute('height', 0.55);
+		s.appendChild(b);
 
-        //top of sweden
-       for(var i = 0; i< 50; i++){
-          // Material Node
-          var mat = document.createElement('Material');
-          // Appearance Node
-          var app = document.createElement('Appearance');
-    
-        if(data[i].year > 700){
-            mat.setAttribute("diffuseColor",red);
-            app.appendChild(mat);
-        }
-        else if(data[i].year > 500){
-          mat.setAttribute("diffuseColor",yellow);
-          app.appendChild(mat);
-        }
-        else{
-          mat.setAttribute("diffuseColor",green);
-          app.appendChild(mat);
-        }
-    
-            var t = document.createElement('Transform');
-            t.setAttribute("translation", xCord.integer({min:-3, max:6}) + " -0.5 " + yCord.integer({min: -5, max: 3}) );
-            var s = document.createElement('Shape');
-    
-            s.appendChild(app);
-    
-            t.appendChild(s);
-            var b = document.createElement('Cylinder');
-            b.setAttribute("radius",0.01)
-            b.setAttribute("height",0.55)
-            s.appendChild(b);
-    
-            var ot = document.getElementById('root');
-            ot.appendChild(t);
-        }
+		var ot = document.getElementById('root');
+		ot.appendChild(t);
+	}
 
+	//top of sweden
+	for (var i = 0; i < 50; i++) {
+		// Material Node
+		var mat = document.createElement('Material');
+		// Appearance Node
+		var app = document.createElement('Appearance');
+
+		if (data[i].year > 700) {
+			mat.setAttribute('diffuseColor', red);
+			app.appendChild(mat);
+		} else if (data[i].year > 500) {
+			mat.setAttribute('diffuseColor', yellow);
+			app.appendChild(mat);
+		} else {
+			mat.setAttribute('diffuseColor', green);
+			app.appendChild(mat);
+		}
+
+		var t = document.createElement('Transform');
+		t.setAttribute(
+			'translation',
+			xCord.integer({ min: -3, max: 6 }) + ' -0.5 ' + yCord.integer({ min: -5, max: 3 })
+		);
+		var s = document.createElement('Shape');
+
+		s.appendChild(app);
+
+		t.appendChild(s);
+		var b = document.createElement('Cylinder');
+		b.setAttribute('radius', 0.01);
+		b.setAttribute('height', 0.55);
+		s.appendChild(b);
+
+		var ot = document.getElementById('root');
+		ot.appendChild(t);
+	}
 }
-
 
 fetchData();
