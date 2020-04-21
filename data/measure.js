7687db8046d77b99e0907b0808aae70a9b297efe
Redines <pontus.g91@gmail.com>
2020-04-21 09:51:17 +0200

    finished function for saving data to downloadable textfile

--- data/measure.js
+++ data/measure.js
@@ -1,2 +1,17 @@
 console.log("Measuring render time");
 
+//Function for saving measured data and downloading it as a plain .txt file
+function saveDatatoFile(artefact,data){
+    console.log("Creating file to save measuredata to");
+    const blob = new Blob([data], {type: "text/plain"});
+    const url = window.URL.createObjectURL(blob);
+    const a = document.createElement("a");
+    a.href = url;
+    a.download = artefact+".txt";
+    a.click();
+}
+
+function readToFile(){
+
+}
+
