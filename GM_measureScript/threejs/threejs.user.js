5b8251ba5b5abdc6df33c48b8b6258e445657b71
Redines <pontus.g91@gmail.com>
2020-04-22 10:18:52 +0200

    removed parametres from saveDatatoFile function because not needed anymore, modified function code accordingly, added variable to retrieve storead measurements from localstorage and store content in text file

--- GM_measureScript/threejs/threejs.user.js
+++ GM_measureScript/threejs/threejs.user.js
@@ -19,15 +19,17 @@ if (window.location.href.endsWith('threejs.html')) {
 	console.log(artefact);
 }
 
-//Function for saving measured data and downloading it as a plain .txt file
-function saveDatatoFile(artefact,data){
-    console.log("Creating file to save measuredata to");
-    const blob = new Blob([data], {type: "text/plain"});
-    const url = window.URL.createObjectURL(blob);
-    const a = document.createElement("a");
-    a.href = url;
-    a.download = artefact+".txt";
-    a.click();
+//Function for saving measured data
+//and downloading it as a plain .txt file
+function saveDatatoFile() {
+	console.log('Creating file to save measuredata to');
+	var data = localStorage.getItem('measure');
+	const blob = new Blob([ data ], { type: 'text/plain' });
+	const url = window.URL.createObjectURL(blob);
+	const a = document.createElement('a');
+	a.href = url;
+	a.download = artefact + '.txt';
+	a.click();
 }
 
 window.addEventListener(
